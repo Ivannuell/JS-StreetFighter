d5be9cdbcd3d836f0ad7292781b60675abd3f920
Ivann <i.herrera.535614@umindanao.edu.ph>
2024-06-05 06:40:12 +0800

    + added stats initaial stat panel without name stat

--- src/javascript/components/fighterPreview.js
+++ src/javascript/components/fighterPreview.js
@@ -16,6 +16,51 @@ export function createFighterImage(fighter) {
     return imgElement;
 }
 
+function createStatBar(stat, value) {
+    const statElement = createElement({
+        tagName: 'div',
+        className: 'fighter-preview___statbar'
+    });
+
+    const statName = createElement({
+        tagName: 'span',
+        className: 'fighter-preview___statbar-name'
+    });
+
+    const statVal = createElement({
+        tagName: 'span',
+        className: 'fighter-preview___statbar-value'
+    });
+
+    statName.innerText = `${stat}: `;
+    statVal.innerText = value;
+
+    statElement.append(statName);
+    statElement.append(statVal);
+
+    return statElement;
+}
+
+function createFighterStatsPanel(fighter, position) {
+    // _id, name, health, attack, defence
+    // const {name, health, attack, defense} = fighter
+    const { health, attack, defense } = fighter;
+    const statsPanel = createElement({
+        tagName: 'div',
+        className: `fighter-preview___root-statPanel ${position}`
+    });
+
+    const healthStat = createStatBar('Health', health);
+    const attackStat = createStatBar('Attack', attack);
+    const defenceStat = createStatBar('Defence', defense);
+
+    statsPanel.append(healthStat);
+    statsPanel.append(attackStat);
+    statsPanel.append(defenceStat);
+
+    return statsPanel;
+}
+
 export function createFighterPreview(fighter, position) {
     const positionClassName = position === 'right' ? 'fighter-preview___right' : 'fighter-preview___left';
     const fighterElement = createElement({
@@ -24,12 +69,17 @@ export function createFighterPreview(fighter, position) {
     });
 
     // todo: show fighter info (image, name, health, etc.)
-    async function foo() {
-        const fighterImage = await createFighterImage(await fighter);
-        return fighterImage;
+
+    async function handlePreviewImage(currFighter) {
+        return createFighterImage(currFighter);
     }
 
-    foo().then(fighterImage => fighterElement.append(fighterImage));
+    async function handlePreviewStatPanel(currFighter, currPosition) {
+        return createFighterStatsPanel(currFighter, currPosition);
+    }
+
+    handlePreviewStatPanel(fighter, positionClassName).then(panel => fighterElement.append(panel));
+    handlePreviewImage(fighter).then(image => fighterElement.append(image));
 
     return fighterElement;
 }
